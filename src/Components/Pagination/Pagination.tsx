4cf9c6fede0269f47558a623c281f1d7b661c2ae
amirabdolazimi <amirabdolazimi.v@gmail.com>
2024-06-20 22:09:20 +0330

    Pagination added to EventSuggestionPage

---
+++ src/Components/Pagination/Pagination.tsx
@@ -0,0 +1,47 @@
+import LeftArrowIcon from "../Icons/LeftArrowIcon";
+import RightArrowIcon from "../Icons/RightArrowIcon";
+import classes from "./Pagination.module.css";
+const Pagination = ({
+  totalPosts,
+  postPerPage,
+  setCurrentPage,
+  currentPage,
+}) => {
+  let pages = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
+    pages.push(i);
+  }
+  return (
+    <div className={classes.pagination}>
+      <button
+        className={`${currentPage === 1 && classes.disabled}`}
+        disabled={currentPage === 1 && true}
+        onClick={() => {
+          setCurrentPage(currentPage - 1);
+        }}
+      >
+        <RightArrowIcon />
+      </button>
+      {pages.map((page, index) => (
+        <button className={`${page === currentPage && classes.active}`} onClick={() => setCurrentPage(page)} key={index}>
+          {page}
+        </button>
+      ))}
+
+      <button
+        className={`${
+          currentPage === pages[pages.length - 1] && classes.disabled
+        }`}
+        disabled={currentPage === pages[pages.length - 1] && true}
+        onClick={() => {
+          setCurrentPage(currentPage + 1);
+        }}
+      >
+        <LeftArrowIcon />
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/Pages/EventSuggestionPage/EventSuggestionPage.tsx
+++ src/Pages/EventSuggestionPage/EventSuggestionPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import FilterIcon from "../../Components/Icons/FilterIcon";
 import JaheshIcon from "../../Components/Icons/JaheshIcon";
 import Post from "../../Components/PostItems/Post";
@@ -7,171 +8,218 @@ import steve from "/images/1.png";
 import pavel from "/images/2.png";
 import ilon from "/images/3.png";
 import img from "/images/pic.jfif";
+import Pagination from "../../Components/Pagination/Pagination";
+const posts = [
+  {
+    id: 0,
+    isWinner: true,
+    personPicture: img,
+    personName: "سمانه احمدی",
+    postDate: dateFormatter("2024-02-07"),
+    postTitle: "پیشنهاد برای چالش کدنویسی همکاران سیستم",
+    postDescription:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است",
+    sliderContent: [
+      {
+        id: 0,
+        image: pavel,
+        alt: "Pavel Durov Picture",
+      },
+      {
+        id: 1,
+        image: steve,
+        alt: "Steve Jobs Picture",
+      },
+      {
+        id: 2,
+        image: ilon,
+        alt: "Ilon mask Picture",
+      },
+    ],
+  },
+  {
+    id: 1,
+    isWinner: false,
+    personPicture: ilon,
+    personName: "Amir ",
+    postDate: dateFormatter("2024-02-08"),
+    postTitle: "پیشنهاد برای چالش دوآپس دیجی کالا",
+    postDescription:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با تست تست تست از طراحان گرافیک است",
+    sliderContent: [
+      {
+        id: 0,
+        image: steve,
+        alt: "Pavel Durov Picture",
+      },
+      {
+        id: 1,
+        image: ilon,
+        alt: "Steve Jobs Picture",
+      },
+      {
+        id: 2,
+        image: pavel,
+        alt: "Ilon mask Picture",
+      },
+    ],
+  },
+  {
+    id: 2,
+    isWinner: false,
+    personPicture: ilon,
+    personName: "Akbar ",
+    postDate: dateFormatter("2024-02-08"),
+    postTitle: "پیشنهاد برای آزمون نهایی دبیرستان",
+    postDescription: "تست پیست سوم ",
+    sliderContent: [
+      {
+        id: 0,
+        image: steve,
+        alt: "Steve Jobs Picture",
+      },
+      {
+        id: 1,
+        image: steve,
+        alt: "Steve Jobs Picture",
+      },
+      {
+        id: 2,
+        image: steve,
+        alt: "Steve Jobs Picture",
+      },
+    ],
+  },
+  {
+    id: 3,
+    isWinner: false,
+    personPicture: ilon,
+    personName: "Asghar ",
+    postDate: dateFormatter("2024-02-08"),
+    postTitle: "پیشنهاد برای چالش تست تسک جهش",
+    postDescription: "این یک متن تست برای تکنیکال تسک جهش است",
+    sliderContent: [
+      {
+        id: 0,
+        image: pavel,
+        alt: "Pavel Durov Picture",
+      },
+      {
+        id: 1,
+        image: pavel,
+        alt: "Pavel Durov Picture",
+      },
+      {
+        id: 2,
+        image: pavel,
+        alt: "Pavel Durov Picture",
+      },
+    ],
+  },
+  {
+    id: 4,
+    isWinner: false,
+    personPicture: ilon,
+    personName: "Hassan ",
+    postDate: dateFormatter("2024-02-08"),
+    postTitle: "پیشنهاد برای چالش 321312321312 همکاران سیستم",
+    postDescription:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با 321312312321312321312312312 از طراحان گرافیک است",
+    sliderContent: [
+      {
+        id: 0,
+        image: steve,
+        alt: "Pavel Durov Picture",
+      },
+      {
+        id: 1,
+        image: pavel,
+        alt: "Steve Jobs Picture",
+      },
+      {
+        id: 2,
+        image: ilon,
+        alt: "Ilon mask Picture",
+      },
+    ],
+  },
+  {
+    id: 5,
+    isWinner: false,
+    personPicture: ilon,
+    personName: "reza ",
+    postDate: dateFormatter("2024-02-08"),
+    postTitle: "پیشنهادی برای این پست ندارم ",
+    postDescription: "برای این قسمت پست نیز پیشنهادی ندارم ",
+    sliderContent: [
+      {
+        id: 0,
+        image: ilon,
+        alt: "Ilon mask Picture",
+      },
+      {
+        id: 1,
+        image: ilon,
+        alt: "Ilon mask Picture",
+      },
+      {
+        id: 2,
+        image: ilon,
+        alt: "Ilon mask Picture",
+      },
+    ],
+  },
+  {
+    id: 6,
+    isWinner: false,
+    personPicture: ilon,
+    personName: "حصین رحمتی",
+    postDate: dateFormatter("2024-02-08"),
+    postTitle: "زندگی نامه حصین رحمتی",
+    postDescription: "متولد دوازدهم مرداد 1366 در شهر مشهد میباشد ",
+    sliderContent: [
+      {
+        id: 2,
+        image: ilon,
+        alt: "Ilon mask Picture",
+      },
+
+      {
+        id: 1,
+        image: pavel,
+        alt: "Steve Jobs Picture",
+      },
+      {
+        id: 0,
+        image: steve,
+        alt: "Pavel Durov Picture",
+      },
+      {
+        id: 0,
+        image: steve,
+        alt: "Pavel Durov Picture",
+      },
+      {
+        id: 0,
+        image: steve,
+        alt: "Pavel Durov Picture",
+      },
+      {
+        id: 0,
+        image: steve,
+        alt: "Pavel Durov Picture",
+      },
+    ],
+  },
+];
+
 const EventSuggestionPage = () => {
-  const posts = [
-    {
-      id: 0,
-      isWinner: true,
-      personPicture: img,
-      personName: "سمانه احمدی",
-      postDate: dateFormatter("2024-02-07"),
-      postTitle: "پیشنهاد برای چالش کدنویسی همکاران سیستم",
-      postDescription:
-        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است",
-      sliderContent: [
-        {
-          id: 0,
-          image: pavel,
-          alt: "Pavel Durov Picture",
-        },
-        {
-          id: 1,
-          image: steve,
-          alt: "Steve Jobs Picture",
-        },
-        {
-          id: 2,
-          image: ilon,
-          alt: "Ilon mask Picture",
-        },
-      ],
-    },
-    {
-      id: 1,
-      isWinner: false,
-      personPicture: ilon,
-      personName: "Amir ",
-      postDate: dateFormatter("2024-02-08"),
-      postTitle: "پیشنهاد برای چالش 321312321312 همکاران سیستم",
-      postDescription:
-        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با 321312312321312321312312312 از طراحان گرافیک است",
-      sliderContent: [
-        {
-          id: 0,
-          image: steve,
-          alt: "Pavel Durov Picture",
-        },
-        {
-          id: 1,
-          image: pavel,
-          alt: "Steve Jobs Picture",
-        },
-        {
-          id: 2,
-          image: ilon,
-          alt: "Ilon mask Picture",
-        },
-      ],
-    },
-    {
-      id: 2,
-      isWinner: false,
-      personPicture: ilon,
-      personName: "Amir ",
-      postDate: dateFormatter("2024-02-08"),
-      postTitle: "پیشنهاد برای چالش 321312321312 همکاران سیستم",
-      postDescription:
-        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با 321312312321312321312312312 از طراحان گرافیک است",
-      sliderContent: [
-        {
-          id: 0,
-          image: steve,
-          alt: "Pavel Durov Picture",
-        },
-        {
-          id: 1,
-          image: pavel,
-          alt: "Steve Jobs Picture",
-        },
-        {
-          id: 2,
-          image: ilon,
-          alt: "Ilon mask Picture",
-        },
-      ],
-    },
-    {
-      id: 3,
-      isWinner: false,
-      personPicture: ilon,
-      personName: "Amir ",
-      postDate: dateFormatter("2024-02-08"),
-      postTitle: "پیشنهاد برای چالش 321312321312 همکاران سیستم",
-      postDescription:
-        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با 321312312321312321312312312 از طراحان گرافیک است",
-      sliderContent: [
-        {
-          id: 0,
-          image: steve,
-          alt: "Pavel Durov Picture",
-        },
-        {
-          id: 1,
-          image: pavel,
-          alt: "Steve Jobs Picture",
-        },
-        {
-          id: 2,
-          image: ilon,
-          alt: "Ilon mask Picture",
-        },
-      ],
-    },
-    {
-      id: 4,
-      isWinner: false,
-      personPicture: ilon,
-      personName: "Amir ",
-      postDate: dateFormatter("2024-02-08"),
-      postTitle: "پیشنهاد برای چالش 321312321312 همکاران سیستم",
-      postDescription:
-        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با 321312312321312321312312312 از طراحان گرافیک است",
-      sliderContent: [
-        {
-          id: 0,
-          image: steve,
-          alt: "Pavel Durov Picture",
-        },
-        {
-          id: 1,
-          image: pavel,
-          alt: "Steve Jobs Picture",
-        },
-        {
-          id: 2,
-          image: ilon,
-          alt: "Ilon mask Picture",
-        },
-      ],
-    },
-    {
-      id: 5,
-      isWinner: false,
-      personPicture: ilon,
-      personName: "Amir ",
-      postDate: dateFormatter("2024-02-08"),
-      postTitle: "پیشنهاد برای چالش 321312321312 همکاران سیستم",
-      postDescription:
-        "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با 321312312321312321312312312 از طراحان گرافیک است",
-      sliderContent: [
-        {
-          id: 0,
-          image: steve,
-          alt: "Pavel Durov Picture",
-        },
-        {
-          id: 1,
-          image: pavel,
-          alt: "Steve Jobs Picture",
-        },
-        {
-          id: 2,
-          image: ilon,
-          alt: "Ilon mask Picture",
-        },
-      ],
-    },
-  ];
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const postPerPage = 2;
+  const lastPostIndex = currentPage * postPerPage;
+  const firstPostIndex = lastPostIndex - postPerPage;
+  const currentPosts = posts.slice(firstPostIndex, lastPostIndex);
 
   return (
     <div className={classes.container}>
@@ -190,9 +238,18 @@ const EventSuggestionPage = () => {
         </div>
       </header>
       <div>
-        {posts.map((post) => (
+        {currentPosts.map((post) => (
           <Post {...post} key={post.id} />
         ))}
+      </div>
+      <div>
+        {/* Pagination Section */}
+        <Pagination
+          totalPosts={posts.length}
+          postPerPage={postPerPage}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        />
       </div>
     </div>
   );
